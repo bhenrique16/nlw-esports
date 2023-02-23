91b6f88daf6d5fdde8257ea77f8af6326992996a
bhenrique16 <bh.henrique16@gmail.com>
2023-02-22 21:13:12 -0300

    biblioteca toggle

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,32 @@
-
+import { useEffect, useState } from 'react'
+import * as Dialog from '@radix-ui/react-dialog'
 import './styles/main.css'
 import logoImg from './assets/logo-nlw-esports.svg'
 import { GameBanner } from './components/GameBanner'
 import { CreateAdBanner } from './components/CreateAdBanner'
 
+
+import { CreateAdModal } from './components/CreateAdModal'
+
+interface Game{
+  id:string;
+  title:string;
+  bannerUrl:string;
+   _count:{
+    ads:number;
+   }
+}
 function App() {
-  console.log('sasds')
-   
+    const [games,setGames] = useState<Game[]>([])
+    useEffect(()=>{
+        fetch('http://localhost:3333/games')
+        .then(response=> response.json())
+        .then(data =>{
+          console.log(data);
+          setGames(data)
+        })
+    },[]
+    )
    return( 
    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
     <img src={logoImg} alt="" />
@@ -15,12 +35,22 @@ function App() {
       Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.
       </h1>
       <div className="grid grid-cols-6 gap-6 mt-16 ">
-       <GameBanner bannerUrl='/game-1.png' title='League o Legends' adsCount={5}/>
-       <GameBanner bannerUrl='/game-2.png' title='Dota2' adsCount={2}/>
-       
+
+        {games.map(game=>{
+          return(
+            <GameBanner
+             key={game.id}
+             bannerUrl={game.bannerUrl} 
+             title={game.title} 
+             adsCount={game._count.ads}/>
+          )
+        })}   
 </div>
+        <Dialog.Root>
       <CreateAdBanner />
+        <CreateAdModal/>
       
+      </Dialog.Root>
    </div>
   )
 
--- web/src/components/CreateAdBanner.tsx
+++ web/src/components/CreateAdBanner.tsx
@@ -1,4 +1,5 @@
 import { MagnifyingGlassPlus } from "phosphor-react";
+import * as Dialog from '@radix-ui/react-dialog'
 
 export function CreateAdBanner (){
   return(  <div className="pt-1 bg-nlw-gradient mt-8 self-stretch rounded-lg overflow-hidden">
@@ -7,9 +8,9 @@ export function CreateAdBanner (){
           <strong className="text-2xl text-white font-black block">Não encontrou seu duo ?</strong>
         <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
         </div>
-        <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+        <Dialog.Trigger className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
           <MagnifyingGlassPlus size={24} />
-            Publicar anúncio.</button>
+            Publicar anúncio.</Dialog.Trigger>
 
        </div>
       </div>
